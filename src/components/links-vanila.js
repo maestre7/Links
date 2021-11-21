d7e136f88f737228833274ee539b9a8d9d50ea7f
maestre7 <maestre7@gmail.com>
2021-11-21 11:30:25 +0100

    start vanila

---
+++ src/components/links-vanila.js
@@ -0,0 +1,29 @@
+﻿
+
+function Theme(newTheme) {
+	let newDivTheme = document.createElement("div");
+	let newClassTheme = document.createAttribute("class");
+	newDivTheme.innerHTML = newTheme;
+	newClassTheme.value = newTheme;
+	newDivTheme.setAttributeNode(newClassTheme);
+	
+	return newDivTheme;
+}
+
+function ElementLink(data) {
+	// let newList = document.createElement("ol");
+	let newListElement = document.createElement("li");
+	let newListElementA = document.createElement("a");
+	let newHref = document.createAttribute("href");
+	let newTarget = document.createAttribute("target");
+	newListElementA.innerHTML = data.name;
+	newHref.value = data.href;
+	newTarget.value = '_blank';
+	newListElement.appendChild(newListElementA);
+	
+	return newListElement;
+}
+
+class Links {
+
+}
