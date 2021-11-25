a57af25e8d0128e303da011ce64b15524f00f70a
Alfredo Ruiz Santos <65159778+maestre7@users.noreply.github.com>
2021-11-25 16:21:32 +0100

    js basic and css basic

--- vanilla/index.js
+++ vanilla/index.js
@@ -1,12 +1,84 @@
 ﻿'use strict';
 //import './components/file/archivos.js'
 //export const data = './data';
-import { data } from './data.js'
+//import { data } from './data.js'
+
+
+const data = {
+	html : [{ name: 'W3schools / HTML',
+		href: 'https://www.w3schools.com/html/default.asp'}, 
+		{ name:'MDN / HTML',
+		href: 'https://developer.mozilla.org/es/docs/Web/HTML'},
+		{ name:'HTML CHEATSHEET',
+		href: 'https://devhints.io/html'},
+		{ name: 'htmlcolorcodes / HTML',
+		href: 'https://htmlcolorcodes.com/es/'},
+		{ name: 'DOM Events / HTML',
+		href: 'https://www.w3schools.com/jsref/dom_obj_event.asp'
+	}],
+	css : [{ name: 'W3schools / CSS',
+		href: 'https://www.w3schools.com/css/default.asp'},
+		{ name: 'MDN / CSS',
+		href: 'https://developer.mozilla.org/es/docs/Web/CSS'},
+		{ name: 'CSS CHEATSHEET / CSS',
+		href: 'https://devhints.io/css'},
+		{ name: 'CSS Border-style / CSS',
+		href: 'https://developer.mozilla.org/es/docs/Web/CSS/border-style'},
+		{ name: 'CSS Border-radius / CSS',
+		href: 'https://www.w3schools.com/cssref/css3_pr_border-radius.asp'},
+		{ name: 'CSS display / CSS',
+		href: 'https://developer.mozilla.org/es/docs/Web/CSS/display'},
+		{ name: 'CSS Propiedades / CSS',
+		href: 'https://www.mclibre.org/consultar/amaya/css/css-propiedades.html'},
+		{ name: 'CSS Transitions / CSS',
+		href: 'https://www.w3schools.com/css/css3_transitions.asp'},
+		{ name: 'CSS Transformaciones / CSS',
+		href: 'https://www.antofernandez.com/transformaciones-css-guia-basica/'},
+		{ name: 'CSS Animations / CSS',
+		href: 'https://www.w3schools.com/css/css3_animations.asp'
+	}],
+	javascript: [{ name:'W3schools / JS',
+		href: 'https://www.w3schools.com/js/default.asp'},
+		{ name: 'MDN / JS',
+		href: 'https://developer.mozilla.org/es/docs/Web/JavaScript'},
+		{ name:'JavaScript CHEATSHEET / JS',
+		href: 'https://devhints.io/es6'		
+	}],
+	reactjs: [{ name: 'React CHEATSHEET / JS',
+		href:'https://devhints.io/react'},
+		{ name: 'Cargar una imagen en React',
+		href: 'https://www.youtube.com/watch?v=ZwSwp8iRk2E'},
+		{ name: 'React Tutorial: An Overview and Walkthrough',
+		href: 'https://www.taniarascia.com/getting-started-with-react/'},
+		{ name: '¿Por qué escribimos super(props)?',
+		href: 'https://overreacted.io/es/why-do-we-write-super-props/'},
+		{ name: 'React Developer Tools - Chrome',
+		href: 'https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=es'},
+		{ name: 'React Developer Tools - FireFox',
+		href: 'https://addons.mozilla.org/es/firefox/addon/react-devtools/'		
+	}],
+	ejercicios: [{ name:'Codewars',
+		href:'https://www.codewars.com/'},
+		{ name:'katacoda',
+		href:'https://www.katacoda.com/courses/git'},
+		{ name: 'learngitbranching',
+		href: 'https://learngitbranching.js.org/?locale=es_ES'},
+		{ name: 'codecombat',
+		href: 'https://codecombat.com/'},
+		{name: 'learngitbranching',
+		href: 'https://learngitbranching.js.org/?locale=es_ES'
+	}],
+	otros: [{name: 'HolaMundo / Youtube - Recursividad, programación funcional en javascript',
+		hreft:'https://www.youtube.com/watch?v=AZd_CzcRhXs' },
+		{name: 'Freecodecamp',
+		href: 'https://www.freecodecamp.org/espanol/news/'
+	}]
+}
 
 function Theme(newTheme) {
 	let newDivTheme = document.createElement("div");
 	let newClassTheme = document.createAttribute("class");
-	newDivTheme.innerHTML = newTheme;
+	newDivTheme.innerHTML = newTheme.toUpperCase();
 	newClassTheme.value = newTheme;
 	newDivTheme.setAttributeNode(newClassTheme);
 	
@@ -22,6 +94,8 @@ function ElementLink(data) {
 	newListElementA.innerHTML = data.name;
 	newHref.value = data.href;
 	newTarget.value = '_blank';
+	newListElementA.setAttributeNode(newHref);
+	newListElementA.setAttributeNode(newTarget);
 	newListElement.appendChild(newListElementA);
 	
 	return newListElement;
@@ -29,30 +103,21 @@ function ElementLink(data) {
 
 class Links {
 	constructor(data){
-		//const data = ReadFile(file);
-		//if (file !== false) {
-		//	this.data = JSON.parse(data);
-		//}
 		this.data = data;
-		console.log(this.data);
 	}
 
 	loadingData() {
         let elementMain = document.querySelector("main");
 		for (let [key, value] of Object.entries(this.data)) {
-			newDivTheme = Theme(key);
-			divTheme = value.array.forEach(element => {
-				newListElement = ElementLink(element);
-				newDivTheme.appendChild(newListElement);
+			let newDivTheme = Theme(key);
 
-				return newDivTheme;
+			value.forEach(element => {
+				let newListElement = ElementLink(element);
+				newDivTheme.appendChild(newListElement);
 			});
-			elementMain.appendChild(divTheme)
+			elementMain.appendChild(newDivTheme);
 		};
 	}
 }
 
 
-
-
-Links(data);
